78e584cfad8cca33a225658918f2d4a7c3dd241c
lipun45dora <64270114+lipun45dora@users.noreply.github.com>
2022-03-21 18:02:05 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { ResultsComponent } from './Pages/home/components/results/results.component';
+import { SearchComponent } from './Pages/home/components/search/search.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResultsComponent,
+    SearchComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
